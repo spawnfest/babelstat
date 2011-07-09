a33b0eed9d1ad124a1e54c719b84a6b584ceaa8c
nisbus <nisbus@kodiak.is>
2011-07-09 16:53:16 +0000

    Added some more tests and a document creator

--- src/babel_calc.erl
+++ src/babel_calc.erl
@@ -10,7 +10,7 @@
 -include("../include/babelstat.hrl").
 %% API
 -export([eval/1,query_db/2]).
--export([test_calculation_parser/0,replace_token_with_value/3]).
+-export([test_calculation_parser/0,replace_token_with_value/3,test_query/0,test_query/3, date_adjust/6,to_babelstat_records/1,test_dates/0]).
 
 %%%===================================================================
 %%% API
@@ -25,12 +25,11 @@ transpose([H|_]=L) -> [lists:map(F, L) || F <- [fun(A) -> lists:nth(N, A) end ||
 
 get_documents(_Params,_Filter) ->
     %%Call db for the view    
-    [].
-
-
+    to_babelstat_records([]).
+  
 %%@doc queries the database recursively and returs a babelstat_series
 -spec query_db(Params :: list(), Filter :: tuple()) -> #babelstat_series{}.		      
-query_db(Params, Filter) ->
+query_db(Params, {_,_,FilterFrequency, From, To}= Filter) ->
 	View = get_documents(Params,Filter),
 	case View of
 	    [Doc] ->
@@ -48,27 +47,141 @@ query_db(Params, Filter) ->
 			calculate(replace_tokens_with_values(Series,Algebra))			
 		end;
 	    [_H|_T] = Docs ->
-		{Values,Dates} = lists:foldl(fun(Doc) ->
-				  Value = proplists:get_value(<<"value">>,Doc,0.0),
-				  Date = proplists:get_value(<<"date">>,Doc,0.0),
-				  {Date,Value}
-			  end,{[],[]},Docs),
-		convert_docs_to_series(Params, Filter, {Values,Dates}, Docs);
+		{Dates, Values} = lists:foldl(fun(Doc,Acc) ->
+						     {Dates, Values} = Acc,
+						     {Dates++[Doc#babelstat.date],Values++[Doc#babelstat.value]}
+					     end,{[],[]},Docs),
+		{FilteredValues, FilteredDates,FilteredDocs} = date_adjust(Values,Dates,Docs, FilterFrequency,From, To),
+		convert_docs_to_series(Params, Filter, {FilteredValues,FilteredDates}, FilteredDocs);
 	    _ ->
 		{error, no_documents_found}
 	end.
 			
-	
+date_adjust(Values, Dates, Frequency, Docs, StartDate, EndDate) ->	
+    {Values,Dates,Docs}.
+    %% case lists:any(fun(D) -> D#babelstat.frequency =/= Frequency end,Docs) of
+    %% 	true ->	    
+    %% 	    ParsedStart = parse_date(list_to_binary(StartDate)),
+    %% 	    ParsedEnd = parse_date(list_to_binary(EndDate)),
+    %% 	    io:format("Frequency mismatch, aggregating~n"),
+    %% 	    %%Dates need to be filtered (aggregated to the frequency)
+    %% 	    ValidDates = date_range:create_range(ParsedStart,ParsedEnd, Frequency),
+    %% 	    Zipped = lists:zip3(Values,Dates,Docs),
+    %% 	    lists:foldl(fun(N,_Acc) ->
+    %% 				{_Match,_Dont} = lists:partition(fun(P) ->
+    %% 								       {_,D,_} = P,
+    %% 								       Range = lists:sublist(ValidDates,N,N+1),
+    %% 								       Dt = parse_date(list_to_binary(D)),
+    %% 								       is_date_in_range(Range, Dt)
+    %% 							       end,Zipped) 
+    %% 				{0.0,undefined,undefined},lists:seq(1,length(ValidDates)) end);
+			    
+			   
 
+
+%% 	    R = [lists:map(F,Zipped) || F= [fun(Z) -> 
+%% 						    io:format("Sub ~p~n",[Z]),
+%% 						    lists:foldl(fun(Y,Sum) ->
+%% 	    								io:format("Sum ~p~n",[Sum]),
+%% %	    								{V,_,_} = Sum,
+%% 	    								io:format("Y ~p~n",[Y])
+%% 	    								%% {V2,D2,Doc2} = Y,
+%% 	    								%% {V+V2,D2,Doc2}
+%% 	    							end,{0.0,[],[]},lists:partition(fun(P) ->
+%% 													io:format("P = ~p~n",[P]),
+%% 	    												{_,D,_} = P,
+%% 	    												Range = lists:sublist(ValidDates,N,N+1),
+%% 	    												is_date_in_range(Range,D)
+%% 	    											end,Zipped))
+%% 	    				end || N <- lists:seq(1,length(ValidDates))]],
+%%	    io:format("Beautiful ~p~n",[R]);
+    %% 	false ->
+    %% 	    {Values,Dates,Docs}
+    %% end.
+
+is_date_in_range(Range, Date)->
+    io:format("Range and date comparison ~p ~p~n",[Range,Date]),
+    case length(Range) of 
+	0 ->
+	    false;
+	1 ->
+	    Date =< hd(Range);	    
+	2 ->
+	    From = hd(Range),
+	    To = hd(lists:reverse(Range)),
+	    case {{Date >= From},{Date =< To}} of
+		{{true},{true}} ->
+		    io:format("Is in range~n"),
+		    true;
+		{_,_} ->
+		    io:format("Not in range~n"),
+		    false
+	    end;
+	_ ->
+	    false
+    end.
+%% is_date_in_range(Range,D) ->    
+%%     Date = parse_date(list_to_binary(D)),
+%%     case length(Range) of 
+%% 	0 ->
+%% 	    false;
+%% 	1 ->
+%% 	    Date =< parse_date(list_to_binary(hd(Range)));	    
+%% 	2 ->
+%% 	    From = parse_date(list_to_binary(hd(Range))),
+%% 	    To = parse_date(list_to_binary(hd(lists:reverse(Range)))),
+%% 	    case {{Date >= From},{Date =< To}} of
+%% 		{true,true} ->
+%% 		    true;
+%% 		{_,_} ->
+%% 		    false
+%% 	    end;
+%% 	_ ->
+%% 	    false
+%%     end.
+    
+	    
+parse_date(<<Y:4/binary,"-",M:1/binary,"-",D:1/binary>>) ->
+    {_,{H,Min,Sec}} = erlang:localtime(),
+    {
+      {list_to_integer(binary_to_list(Y)),
+       list_to_integer(binary_to_list(M)),
+       list_to_integer(binary_to_list(D))},
+       {H,Min,Sec}
+      };
+
+parse_date(<<Y:4/binary,"-",M:1/binary,"-",D:2/binary>>) ->
+    {_,{H,Min,Sec}} = erlang:localtime(),
+      {{list_to_integer(binary_to_list(Y)),
+      list_to_integer(binary_to_list(M)),
+       list_to_integer(binary_to_list(D))},
+       {H,Min,Sec}};
+
+parse_date(<<Y:4/binary,"-",M:2/binary,"-",D:2/binary>>) ->
+    {_,{H,Min,Sec}} = erlang:localtime(),
+      {{list_to_integer(binary_to_list(Y)),
+       list_to_integer(binary_to_list(M)),
+       list_to_integer(binary_to_list(D))},
+       {H,Min,Sec}};
+parse_date(<<Y:4/binary,"-",M:2/binary,"-",D:2/binary," ",H:2/binary,":",Min:2/binary,":",Sec:2/binary>>) ->
+      {{list_to_integer(binary_to_list(Y)),
+       list_to_integer(binary_to_list(M)),
+	list_to_integer(binary_to_list(D))},
+       {list_to_integer(binary_to_list(H)),
+	list_to_integer(binary_to_list(Min)),
+	list_to_integer(binary_to_list(Sec))}}.
+    
 
 convert_docs_to_series([Category, Sub_Category, Subject, Series_Category, Title] = Params, {Metric, Scale, Frequency, _, _} = Filter, {Values, Dates}, Docs) ->
-    ConvertedValues = lists:foldl(fun(Doc, Acc) ->
-					  DocScale = proplists:get_value(<<"scale">>,Doc,1),
-					  DocMetric = proplists:get_value(<<"metric">>,Doc,undefined),
-					  Value = lists:seq(length(Acc)+1,Values),
-					  NewValue = convert_scale(DocScale, Scale, Value),
-					  convert_metric(DocMetric, Metric,NewValue)      
-		       end,[],Docs),
+    {ConvertedValues,_} = lists:foldl(fun(Doc, Acc) ->
+					      {NewValues, Counter} = Acc,
+					      DocScale = Doc#babelstat.scale,
+					      DocMetric = Doc#babelstat.metric,
+					      Value = lists:nth(Counter+1,Values),      
+					      NewValue =  convert_scale(DocScale, Scale, Value),
+					      Converted = convert_metric(DocMetric, Metric,NewValue),
+					      {NewValues++[Converted],Counter+1} 
+		       end,{[],0},Docs),
 
     #babelstat_series{dates = Dates, values = ConvertedValues, metric = Metric, scale = Scale, 
 		      frequency = Frequency, category = Category, sub_category = Sub_Category, 
@@ -183,3 +296,88 @@ simplify_algebra(Tokens,Calculation) ->
 				replace(Acc,Token,Char)
 			end,Calculation,lists:seq(1,TokenCount))
     end.
+
+
+to_babelstat_records(Docs) ->
+    lists:map(fun(X) ->
+		      to_babelstat_record(X)
+	      end,Docs).
+
+to_babelstat_record({[{<<"_id">>,Id},{<<"_rev">>,Rev},{<<"date">>,Date},{<<"value">>, Value},
+		     {<<"metric">>, Metric}, {<<"scale">>, Scale}, {<<"frequency">>,Frequency},
+		     {<<"location">>,Location}, {<<"category">>,Cat}, {<<"sub_category">>,SubCat},
+		     {<<"subject">>,Subject}, {<<"series_category">>,SerCat}, {<<"title">>,Title},
+		     {<<"source">>,Source}, {<<"calculation">>,Calculation}, {<<"constant">>,Constant}]}) ->
+    #babelstat{id = Id, rev = Rev, date = binary_to_list(Date), value = Value, 
+	       metric = binary_to_list(Metric), scale = Scale, 
+	       frequency = binary_to_atom(Frequency,latin1),
+	       location = binary_to_list(Location), category = binary_to_list(Cat), 
+	       sub_category = binary_to_list(SubCat), subject = binary_to_list(Subject), 
+	       series_category = binary_to_list(SerCat), title = binary_to_list(Title), source = Source,
+	       calculation = Calculation, constant = Constant}.
+
+test_query() ->
+    Params = ["Spawnfest","Teams","Jesus don't want me for a sunBEAM","code","number of lines"],
+    Filter = {"unit", 1, daily, "2000-01-01", "2000-01-20"},    
+    View = to_babelstat_records(document_creator:get_docs()),
+    case View of
+	[Doc|[]] ->
+	    %%Single document returned, either it is a constant or a calculation
+	    case Doc#babelstat.constant of
+		true ->
+		    create_constants_series(Params, Filter,Doc#babelstat.value,Doc#babelstat.scale,Doc#babelstat.metric);
+		false ->
+		    %%it's a calculation
+		    Calc = Doc#babelstat.calculation,
+		    {Queries, Algebra} = parse_calculation(Calc),
+		    Series = lists:map(fun(Serie) ->
+					       query_db(Serie,Filter)
+				       end,Queries),
+		    calculate(replace_tokens_with_values(Series,Algebra))			
+	    end;
+	[_H|_T] = Docs ->
+	    {Dates,Values} = lists:foldl(fun(Doc, Acc) ->
+						 {Dates, Values} = Acc,
+						 {Dates++[Doc#babelstat.date],Values++[Doc#babelstat.value]}     
+					 end,{[],[]},Docs),
+	    convert_docs_to_series(Params, Filter, {Values,Dates}, Docs);
+	_ ->
+	    {error, no_documents_found}
+    end.
+
+test_query(Measurement,Scale, Frequency) ->
+    Params = ["Spawnfest","Teams","Jesus don't want me for a sunBEAM","code","number of lines"],
+    Filter = {"cm", 1, daily, "2000-01-01", "2002-01-01"},    
+    View = to_babelstat_records(document_creator:get_docs(Measurement, Scale,Frequency)),
+    case View of
+	[Doc|[]] ->
+	    %%Single document returned, either it is a constant or a calculation
+	    case Doc#babelstat.constant of
+		true ->
+		    create_constants_series(Params, Filter,Doc#babelstat.value,Doc#babelstat.scale,Doc#babelstat.metric);
+		false ->
+		    %%it's a calculation
+		    Calc = Doc#babelstat.calculation,
+		    {Queries, Algebra} = parse_calculation(Calc),
+		    Series = lists:map(fun(Serie) ->
+					       query_db(Serie,Filter)
+				       end,Queries),
+		    calculate(replace_tokens_with_values(Series,Algebra))			
+	    end;
+	[_H|_T] = Docs ->
+	    {Dates,Values} = lists:foldl(fun(Doc, Acc) ->
+						 {Dates, Values} = Acc,
+						 {Dates++[Doc#babelstat.date],Values++[Doc#babelstat.value]}     
+					 end,{[],[]},Docs),
+	    convert_docs_to_series(Params, Filter, {Values,Dates}, Docs);
+	_ ->
+	    {error, no_documents_found}
+    end.
+
+test_dates() ->
+    Docs = document_creator:get_docs(<<"cm">>,1,<<"daily">>),
+    Records = to_babelstat_records(Docs),
+    [{Values, Dates}] = lists:map(fun(X) -> {_,D,V,_,_,_,_,_,_,_,_,_,_,_} = X, {V,D} end,[babel_calc:test_query(<<"feet">>,100,<<"daily">>)]),
+    babel_calc:date_adjust(Values,Dates,weeks, Records,hd(Dates),lists:nth(length(Dates),Dates)).
+
+    
---
+++ src/document_creator.erl
@@ -0,0 +1,77 @@
+%%%-------------------------------------------------------------------
+%%% @author nisbus <>
+%%% @copyright (C) 2011, nisbus
+%%% @doc
+%%% Used for testing purposes only
+%%% @end
+%%% Created :  9 Jul 2011 by nisbus <>
+%%%-------------------------------------------------------------------
+-module(document_creator).
+
+%% API
+-export([get_docs/0, get_docs/3]).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+get_docs() ->
+    lists:map(fun(N) ->
+		     create_doc(N)
+	      end,lists:seq(1,20)).
+		  
+get_docs(Metric,Scale,Frequency) ->
+    lists:map(fun(N) ->
+		     create_doc(Metric, Scale, Frequency,N)
+	      end,lists:seq(1,20)).
+    
+%%--------------------------------------------------------------------
+%% @doc
+%% @spec
+%% @end
+%%--------------------------------------------------------------------
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+create_doc(N) ->
+    Date = "2000-1-"++integer_to_list(N),
+    {[
+      {<<"_id">>,N},
+      {<<"_rev">>,N},
+      {<<"date">>,list_to_binary(Date)},
+      {<<"value">>, N*1.0},
+      {<<"metric">>, <<"unit">>},
+      {<<"scale">>, 1},
+      {<<"frequency">>,<<"daily">>},
+      {<<"location">>,<<"[444,33]">>},
+      {<<"category">>,<<"Spawnfest">>},
+      {<<"sub_category">>,<<"Teams">>},
+      {<<"subject">>,<<"Jesus don't want me for a sunBEAM">>},
+      {<<"series_category">>,<<"code">>},
+      {<<"title">>,<<"number of lines">>},
+      {<<"source">>,<<"pure fiction">>},
+      {<<"calculation">>,<<"false">>},
+      {<<"constant">>,<<"false">>}
+     ]}.
+
+create_doc(Metric, Scale, Frequency,N) -> 
+    Date = "2000-1-"++integer_to_list(N),
+    {[
+      {<<"_id">>,N},
+      {<<"_rev">>,N},
+      {<<"date">>,list_to_binary(Date)},
+      {<<"value">>, N*1.0},
+      {<<"metric">>, Metric},
+      {<<"scale">>, Scale},
+      {<<"frequency">>,Frequency},
+      {<<"location">>,<<"[444,33]">>},
+      {<<"category">>,<<"Spawnfest">>},
+      {<<"sub_category">>,<<"Teams">>},
+      {<<"subject">>,<<"Jesus don't want me for a sunBEAM">>},
+      {<<"series_category">>,<<"code">>},
+      {<<"title">>,<<"number of lines">>},
+      {<<"source">>,<<"pure fiction">>},
+      {<<"calculation">>,<<"false">>},
+      {<<"constant">>,<<"false">>}
+     ]}.
+    
