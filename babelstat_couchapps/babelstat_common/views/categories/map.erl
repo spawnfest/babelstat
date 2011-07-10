3f78f4f0e40d105edfd603d738bd49305cabf753
nisbus <nisbus@kodiak.is>
2011-07-10 23:44:35 +0000

    Changed the categories map and the couchapprc

--- babelstat_couchapps/babelstat_common/views/categories/map.erl
+++ babelstat_couchapps/babelstat_common/views/categories/map.erl
@@ -1,7 +1,7 @@
 fun({Doc}) ->
         case proplists:get_value(<<"type">>, Doc, null) of
             null ->
-                undefined;
+                <<"ignore me">>;
             _ ->
                 Category = proplists:get_value(<<"category">>, Doc),
                 Emit(Category, 1)
