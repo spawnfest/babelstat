4cf201c93a2786b0e301921841144e2c749b93e0
Ómar Kjartan Yasin <omarkj@gmail.com>
2011-07-10 02:35:25 +0000

    typo

--- src/babelstat_sup.erl
+++ src/babelstat_sup.erl
@@ -11,8 +11,8 @@ start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-    CalculationSupSup = {babelstat_calculation_sup_sup,
-			 {babelstat_calculation_sup_sup, start_link, []},
-			 permanent, 5000, supervisor, [babelstat_calculation_sup_sup]},
-    {ok, {{one_for_one, 5, 20}, [CalculationSupSup]}}.
+    CalculationSup = {babelstat_calculation_sup,
+			 {babelstat_calculation_sup, start_link, []},
+			 permanent, 5000, supervisor, [babelstat_calculation_sup]},
+    {ok, {{one_for_one, 5, 20}, [CalculationSup]}}.
 
