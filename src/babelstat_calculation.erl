3169ba6a3bc01489bbac2b56f0f3d82ed0ec5c63
Ómar Kjartan Yasin <omarkj@gmail.com>
2011-07-10 02:32:32 +0000

    Try it out

---
+++ src/babelstat_calculation.erl
@@ -0,0 +1,29 @@
+-module(babelstat_calculation).
+
+-behaviour(gen_server).
+
+-export([start_link/3,
+	 init/1,
+%	 handle_call/3,
+%	 handle_cast/2,
+	 handle_info/2,
+%	 code_change/3,
+	 terminate/2]).
+
+-record(state, {
+	  worker :: pid()
+	 }).
+
+start_link(Query, Filter, Callback) ->
+    gen_server:start_link(?MODULE, [Query, Filter, Callback], []).
+
+init([Query, Filter, Callback]) ->
+    {ok, Pid} = babelstat_calculation_worker:start_link(Query, Filter, Callback),
+%    erlang:monitor(process, Pid),
+    {ok, #state{ worker = Pid }}.
+
+handle_info({'EXIT', _Ref, process, Pid, _Reason}, #state{ worker = Pid } = State) ->
+    {stop, normal, State}.
+
+terminate(_Reason, _State) ->
+    ok.
